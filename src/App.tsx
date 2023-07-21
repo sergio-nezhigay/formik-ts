22000697cc0b0a09ca96bac13c031a137b53a118
Sergio Nezhigay <abc1971h@gmail.com>
2023-07-21 14:45:31 +0300

    hw done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import { Formik, Field, Form, ErrorMessage, FieldProps } from "formik";
+import * as Yup from "yup";
+import InputMask from "react-input-mask";
+import styles from "./App.module.css";
+
+interface FormValues {
+  name: string;
+  email: string;
+  phoneNumber: string;
+}
+
+const validationSchema = Yup.object().shape({
+  name: Yup.string().required("Name is required"),
+  email: Yup.string().email("Invalid email").required("Email is required"),
+  phoneNumber: Yup.string()
+    .test("len", "Phone number must be exactly 12 digits", (val) => {
+      return (val?.match(/\d/g) || []).length === 12;
+    })
+    .required("Phone number is required"),
+});
+
+const initialValues: FormValues = {
+  name: "",
+  email: "",
+  phoneNumber: "",
+};
+
+const App: React.FC = () => {
+  const handleSubmit = (values: FormValues, { setSubmitting }: any) => {
+    console.log("Form values:", values);
+
+    setTimeout(() => {
+      setSubmitting(false);
+    }, 1000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.formContainer}>
+      <h1>Formik Example</h1>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <div>
+              <label htmlFor="name" className={styles.label}>
+                Name:
+              </label>
+              <Field type="text" name="name" className={styles.inputField} />
+              <ErrorMessage
+                name="name"
+                component="div"
+                className={styles.error}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="email" className={styles.label}>
+                Email:
+              </label>
+              <Field type="email" name="email" className={styles.inputField} />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={styles.error}
+              />
+            </div>
+
+            <div>
+              <label htmlFor="phoneNumber" className={styles.label}>
+                Phone Number:
+              </label>
+              <Field name="phoneNumber">
+                {({ field }: FieldProps) => (
+                  <InputMask
+                    {...field}
+                    mask="+38 (099) 999-99-99"
+                    maskChar=" "
+                    className={styles.inputField}
+                  />
+                )}
+              </Field>
+              <ErrorMessage
+                name="phoneNumber"
+                component="div"
+                className={styles.error}
+              />
+            </div>
+
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className={styles.submitButton}
+            >
+              {isSubmitting ? "Submitting..." : "Submit"}
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
-}
+};
 
 export default App;
